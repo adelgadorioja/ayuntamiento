b2d08a9238fb029f67f08bc3bdeafa5a4ca6d97b
adelgadorioja <adelgado.sab@gmail.com>
2018-02-11 16:30:14 +0100

    Vistas y migraciones arregladas

--- resources/views/denuncias/crear.blade.php
+++ resources/views/denuncias/crear.blade.php
@@ -1,57 +1,73 @@
-@extends('layouts.master')
+@extends('layouts.master') @section('content')
 
-@section('content')
+<script>
+    $(document).ready(function () {
+        $('input[value=Enviar]').click(function () {
+            $('#formularioDenuncia')[0].reset();
+            $('#formularioDenuncia').before(
+                '<div class="col-md-12 alert alert-success alert-dismissable">' +
+                '<button type="button" class="close" ' +
+                'data-dismiss="alert" aria-hidden="true">' +
+                '&times;' +
+                '</button>' +
+                'La denuncia se ha tramitado correctamente. Gracias.' +
+                '</div>');
+        });
+
+        $('input[type=file]').change(function () {
+            var archivo = $('input[type=file]').val().split('\\').pop();
+            $('#archivo').text(archivo);
+        })
+    });
+</script>
+
 <div class="container">
 
     <div id="denuncias" class="caja">
         <h5>Realizar una denuncia</h5>
-       
-        <form method="POST" action="{{url('denuncias/crear')}}">
-        {{ csrf_field() }}
-           <input type="hidden" name="id_user" value="{{Auth::id()}}"> 
-           <input type="hidden" name="atendidoPor" value="0"> 
-           <input type="hidden" name="fecha" value="{{date('Y-m-d H:i:s')}}">        
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>Título</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput1" name="titulo">
+
+        <form id="formularioDenuncia" method="POST" action="{{url('denuncias/crear')}}">
+            {{ csrf_field() }}
+            <input type="hidden" name="id_user" value="{{Auth::id()}}">
+            <input type="hidden" name="atendidoPor" value="0">
+            <input type="hidden" name="fecha" value="{{date('Y-m-d H:i:s')}}">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Título</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" name="titulo">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Descripción</label>
+                        <textarea class="descripcion form-control" rows="5" name="descripcion"></textarea>
+                    </div>
                 </div>
-                 
-                <div class="form-group">
-                    <label>Descripción</label>
-                    <textarea class="descripcion form-control" rows="5" name="descripcion"></textarea>
+
+                <div class="col-md-6">
+                    <div class="panel-body">
+                        <div id="adjuntarArchivo">
+                            <label>Adjuntar archivo</label>
+                            <div class="form-group">
+                                <div class="form-control custom-file">
+                                    <input type="file" class="custom-file-input">
+                                    <label id="archivo" class="custom-file-label">Selecciona el archivo</label>
+                                </div>
+                            </div>
+                        </div>
+                        <label>O arrástrelo</label>
+                        <div class="upload-drop-zone" id="drop-zone">
+                            Arrastre su archivo aquí
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group">
-                    <label>Localización</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput1" name="localización">
+                <label>Localización</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="localización">
             </div>
-
-            <div class="col-md-6">
-                <div class="panel-body">
-                    <div id="adjuntarArchivo">
-                        <label>Selecciona el archivo</label>                             
-                            <input type="text" class="form-control" name="imagen">                     
-                        </div>
-                    </div>
-                    <label>O arrástrelo</label>
-                    <div class="upload-drop-zone" id="drop-zone">
-                        Arrastre su archivo aquí
-                    </div>
-            </div>
-            
-            <div class="col-md-12 js-upload-finished">
-                <label>Archivos adjuntos</label>
-                <div class="list-group">
-                    <a class="list-group-item">
-                        <span class="badge alert-success pull-right">jpg</span> imagen1</a>
-                    <a class="list-group-item">
-                        <span class="badge alert-success pull-right">png</span> imagen2</a>
-                </div>
-                <input class="btn btn-100" type="submit" value="Enviar">
-            </div>
+            <input class="btn btn-100" type="button" value="Enviar">
         </form>
     </div>
-</div>    
-@stop
+    @stop
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
+<script>
+    $(document).ready(function () {
+    $('#imagenPortada').outerHeight($('#noticias').outerHeight());
+    var imagenPortada = $('#imagenPortada');
+    var backgrounds = [
+        "{{asset('imagenes/people.jpg')}}", 
+        "{{asset('imagenes/people2.jpg')}}",
+        "{{asset('imagenes/people3.jpg')}}"];
+    var current = 0;
+
+    function nextBackground() {
+        imagenPortada.css(
+            'background-image',
+            backgrounds[current = ++current % backgrounds.length]);
+
+        setTimeout(nextBackground, 5000);
+    }
+    setTimeout(nextBackground, 5000);
+    imagenPortada.css('background-image', backgrounds[0]);
+});
+</script>
  <div class="container">
             <div id="inicioSuperior" class="row">
                 <div class="col-md-8">
