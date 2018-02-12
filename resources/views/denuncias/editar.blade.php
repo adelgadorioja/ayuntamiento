0f97a47da8a20b55fc18fe6564d4d1015d195068
Álvaro <adelgado.sab@gmail.com>
2018-02-12 19:51:07 +0100

    ...

--- resources/views/denuncias/editar.blade.php
+++ resources/views/denuncias/editar.blade.php
@@ -9,7 +9,8 @@
         <p><span>Título:</span> {{$denuncia->titulo}} </p>
         <p><span>Descripción:</span> {{$denuncia->descripcion}} </p>
         <p><span>Imagen:</span> </p>
-        <img src="localhost:8000/storage/{{$denuncia->imagen}}" class="img-fluid">
+        
+        <img src="/storage/{{$denuncia->imagen}}" class="img-fluid">
         <p><span>Localización:</span> {{$denuncia->localizacion}}</p>
 	</div>
 </div>
